3d0a32db837228d773a32db1a5c818dd2a31571b
Cheolho Choo <choo.cheolho@gmail.com>
2019-03-08 17:17:39 +0900

    add commit

---
+++ 20190306/index.js
@@ -0,0 +1,20 @@
+const chooInfo = {
+  name: "choo",
+  age: 33,
+  gender: "Male",
+  isHandosme: true,
+  favMovies: ["보헤미안랩소디", "어벤져스", "스파이더맨", "아이언맨"],
+  favFood: [
+    {
+      name: "Chicken",
+      fatty: false
+    },
+    {
+      name: "Cheese burger",
+      fatty: true
+    }
+  ]
+};
+
+console.log(chooInfo);
+console.log(chooInfo.favFood[0].name);
